18b29c996c86f3c3e79e623a9ff412e30e5a1fb9
“arisa-hirata” <arisa.pirata@gmail.com>
2019-11-02 11:58:32 -0700

    fetched unsplush api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './component/SearchBar';
 
 class App extends React.Component {
   onSearchSubmit(term) {
-    console.log(term);
+    axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term },
+      headers: {
+        Authorization:
+          'Client-ID 6cddb0749cf26a1ff4bffaab6d041a8ac49ff7cb90ac87ad96b9e957531c89f7'
+      }
+    });
   }
-
   render() {
     return (
       <div>
